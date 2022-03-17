8425ccaf818600f046444bfba5fbd2759b1e35a6
cjackson-variant <70218217+cjackson-variant@users.noreply.github.com>
2022-03-17 17:17:06 -0400

    Removed extra tags

--- microservice/incoming_sqs.tf
+++ microservice/incoming_sqs.tf
@@ -1,12 +1,3 @@
-module "tags" {
-  source = "github.com/variant-inc/lazy-terraform//submodules/tags?ref=v1"
-
-  user_tags    = var.user_tags
-  octopus_tags = var.octopus_tags
-
-  name = var.name
-}
-
 data "aws_sns_topic" "incoming_topic_data" {
   name = var.incoming_topic_name
 }
