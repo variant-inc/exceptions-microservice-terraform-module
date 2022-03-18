648fe0ced117e03d7fe6dcc5337bebcb9acbe1d0
cjackson-variant <70218217+cjackson-variant@users.noreply.github.com>
2022-03-18 11:06:20 -0400

    Removed empty config

--- microservice/main.tf
+++ microservice/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "s3" {}
+#   backend "s3" {}
   required_providers {
     aws = {
       version = "~> 3.49.0"
@@ -20,7 +20,7 @@ provider "aws" {
   }
 }
 
-provider "helm" {}
+# provider "helm" {}
 
 provider "kubernetes" {}
 
